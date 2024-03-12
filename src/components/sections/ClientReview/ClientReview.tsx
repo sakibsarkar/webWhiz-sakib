b013cfb9c2bf36c878a1f51f0a031a542f78878f
sakibsarkar <sakibsarkar707@gmail.com>
2024-03-12 21:08:35 +0600

    client review responsive

--- src/components/sections/ClientReview/ClientReview.tsx
+++ src/components/sections/ClientReview/ClientReview.tsx
@@ -8,30 +8,52 @@ const ClientReview = () => {
     return (
         <section className="xl:mt-[76px]">
             <div className="container flex items-start justify-start">
-                <div className="w-[1290px] h-[401px] flex justify-between">
+                <div className="w-full xl:w-[1290px] xl:h-[401px] flex flex-col gap-[24px] items-center md:justify-around xl:flex-row">
 
                     {/* left section */}
-                    <div className="xl:w-[802px] h-full flex items-end justify-between">
-                        <div className="h-full flex flex-col items-end justify-between gap-[32px]">
-                            <div className="">
+                    <div className="sm:w-full xl:w-[802px] h-full flex items-end justify-center sm:gap-[50px] md:justify-around xl:justify-between xl:gap-[32px]">
+                        <div className="h-full xl:w-[464px] flex flex-col items-start justify-between sm:items-end gap-[32px]">
+
+                            <div className="flex xl:items-end">
                                 <SectionHeadingV2 heading="Client Reviews" description="The team at consistently delivered outstanding surpassing expectations" dWidth={314} />
                             </div>
 
-                            {/* small images */}
-                            <div className="flex items-center justify-start xl:gap-[32px]">
 
-                                <div className="xl:w-[216] xl:h-[212px]">
+                            {/* small images for md device */}
+                            <div className="hidden sm:flex items-center justify-start gap-[10px] md:gap-[32px] xl:w-full xl:justify-between">
+
+                                <div className="w-[105px] h-[110px] md:h-[230px] md:w-[200px] xl:w-[216px] xl:h-[212px]">
                                     <Image width={216} height={212} alt="client" src={"/images/Rectangle60.png"} className="imgFit" />
                                 </div>
 
-                                <div className="xl:w-[216] xl:h-[212px]">
+                                <div className="w-[105px] h-[110px] md:h-[230px] md:w-[200px] xl:w-[216px] xl:h-[212px]">
                                     <Image width={216} height={212} alt="client" src={"/images/Rectangle61.png"} className="imgFit" />
                                 </div>
                             </div>
+
+
                         </div>
 
                         {/* big image */}
-                        <div className="xl:w-[306px] xl:h-[401px]">
+                        <div className=" hidden sm:inline-block h-[160px] w-[120px] md:w-[290px] md:h-[380px] xl:w-[306px] xl:h-[401px]">
+                            <Image className="imgFit" alt="bigClient" width={306} height={401} src={"/images/Rectangle62.png"} />
+                        </div>
+
+                    </div>
+
+                    {/* small images and big image for small(mobile) device */}
+                    <div className="sm:hidden flex items-end gap-[10px]">
+                        <div className="flex items-center justify-start gap-[10px] ">
+
+                            <div className="w-[105px] h-[110px]">
+                                <Image width={216} height={212} alt="client" src={"/images/Rectangle60.png"} className="imgFit" />
+                            </div>
+
+                            <div className="w-[105px] h-[110px]">
+                                <Image width={216} height={212} alt="client" src={"/images/Rectangle61.png"} className="imgFit" />
+                            </div>
+                        </div>
+                        <div className="h-[160px] w-[120px] md:w-[290px] md:h-[380px] xl:w-[306px] xl:h-[401px]">
                             <Image className="imgFit" alt="bigClient" width={306} height={401} src={"/images/Rectangle62.png"} />
                         </div>
 
@@ -39,7 +61,7 @@ const ClientReview = () => {
 
                     {/* right section review box */}
 
-                    <div className="xl:w-[456px] h-full flex flex-col justify-end gap-[16px]">
+                    <div className="w-[80%] sm:w-[70%] md:w-[80%] xl:w-[456px] h-full flex flex-col justify-end gap-[16px]">
 
                         <div className="flex justify-end items-center gap-[12px]">
                             <button className="w-[32px] h-[32px] center border-[1px] border-primary rounded-full"><ArrowLeft /></button>
@@ -50,7 +72,7 @@ const ClientReview = () => {
                             <button className="gradient w-[32px] h-[32px] center rounded-full"><ArrowRight /></button>
                         </div>
 
-                        <div className="p-[16px] w-full h-[170px] border-[1px] border-[#5F90F0] rounded-[12px] sm:p-[24px] col-center gap-[16px]">
+                        <div className="p-[16px] w-full h-[170px] border-[1px] border-[#5F90F0] rounded-[12px] sm:p-[24px] flex flex-col justify-between items-center gap-[16px]">
                             <div className="w-full flex justify-between items-center">
                                 <div className="flex flex-col items-start justify-start gap-[2px]">
                                     <h3 className="text-[20px] font-[600] text-[#130929]">Maria Sarapavoo</h3>
--- src/components/shared/SectionHeadingV2.tsx
+++ src/components/shared/SectionHeadingV2.tsx
@@ -1,18 +1,18 @@
 type DataType = {
     heading: string,
     description: string,
-    dWidth: Number
+    dWidth?: Number
 }
 
 const SectionHeadingV2: React.FC<DataType> = ({ heading, description, dWidth }) => {
     return (
         <div className="w-full flex flex-col items-start justify-start gap-[12px]">
-            <h2 className="text-[40px] text-headingColor font-[700] leading-[140%]">{heading}</h2>
+            <h2 className="text-[24px] md:text-[28px] lg:text-[40px] text-headingColor font-[700] leading-[140%]">{heading}</h2>
             {
                 dWidth ?
-                    <p className="verticalHigh w-[323px] text-[14px]  h-[52px] xl:text-[16px] text-paragraphColor leading-[160%]" style={{ width: dWidth + "px" }}>{description}</p>
+                    <p className="verticalHigh  text-[14px]  h-[52px] xl:text-[16px] text-paragraphColor leading-[160%]" style={{ width: dWidth + "px" }}>{description}</p>
                     :
-                    <p className="verticalHigh w-[323px] text-[14px] xl:w-[452px] h-[52px] xl:text-[16px] text-paragraphColor leading-[160%]">{description}</p>
+                    <p className="verticalHigh w-[323px] headV2-p text-[14px] xl:w-[452px] h-[52px] xl:text-[16px] text-paragraphColor leading-[160%]">{description}</p>
             }
         </div>
     );
